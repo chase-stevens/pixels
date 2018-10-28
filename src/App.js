6bda7ae01562a21ff6ad2bf61f0f461fb9231155
chase-stevens <chasestevenspersonal@gmail.com>
2018-10-27 22:17:09 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+class Pixel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOn: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isOn: !state.isOn
+    }));
+  }
+
+  render() {
+    return <div
+              className={this.state.isOn ? 'Pixel Blue' : 'Pixel Red'}
+              onClick={this.handleClick}
+            ></div>
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="Welcome">
+          <Welcome name="Chase" />
+        </div>
+        <div><Pixel /><Pixel /><Pixel /><Pixel /></div>
+        <div><Pixel /><Pixel /><Pixel /><Pixel /></div>
+        <div><Pixel /><Pixel /><Pixel /><Pixel /></div>
+        <div><Pixel /><Pixel /><Pixel /><Pixel /></div>
       </div>
     );
   }
